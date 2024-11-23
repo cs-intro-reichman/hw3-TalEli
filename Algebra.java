8d47a57b6c8f377ab52e9808007618c478efa537
tal <tal.ariel@runi.post.ac.il>
2024-11-23 21:29:51 +0200

    alg

--- Algebra.java
+++ Algebra.java
@@ -15,53 +15,89 @@ public class Algebra {
    		System.out.println(pow(3,5));      // 3^5
    		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
+   	    System.out.println(div(25,7));   // 25 / 7 =3
+   		System.out.println(mod(25,7));   // 25 % 7 = 4
    		System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
+	 	System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+		x1++; 
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+			x1--; 
+		}
+		return x1;
+
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = 0;
+		if (x1 == 0 || x2 == 0 )
+		{
+			return 0;
+		}
+		for (int i = 0; i < x2; i++) {
+			sum = plus(sum, x1);
+		}
+		//
+	return sum;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = 1;
+		for (int i = 0; i < n; i++){
+			sum = times(sum, x);
+		}
+				return sum;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		if (x1 == 0 || x2 == 0){
+			return 0;
+		}
+		int n  = 1; 
+		while (x1 > times(x2, n)){
+			n++;
+		}
+		return n;
+		}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		
+		int n=0; 
+		while (times(n, x2) < x1)
+		{
+			n++;
+		}
+		if (times(n, x2) == x1){
+			return 0; 
+		}
+		else {
+			return (minus(times(n, x2), x1));
+			}
+		}
+		
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int n = 1;
+		while (pow(n, 2) <= x){
+			n++;
+		}
+		return n;
 	}	  	  
 }
